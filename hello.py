054f87c3cf02d54e194b3a42878f0b4deab962f0
HZC <815547360@qq.com>
2017-05-20 01:51:50 +0800

    Dynamic routes

--- hello.py
+++ hello.py
@@ -4,9 +4,13 @@ __author__ = 'HZC'
 from flask import Flask
 app = Flask(__name__)
 
+#拦截路由
 @app.route('/')
 def index():
 	return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s</h1>' % name
 # 确保直接执行这个脚本时才启动开发Web服务器
 if __name__ == '__main__':
 	app.run(debug=True) #启动调试模式设置debug为True
