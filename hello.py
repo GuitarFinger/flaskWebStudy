2f842cf76b998ed4b18145a0b99e79e3fa904276
HZC <815547360@qq.com>
2017-05-24 23:32:52 +0800

    Jinja2 Templates

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*-
 __author__ = 'HZC'
-from flask import Flask
+from flask import Flask, render_template  # 渲染模板模块render_template
 from flask_script import Manager  # 为flask程序添加了一个命令行解析器
 
 app = Flask(__name__)  # 程序实例是Flask类的对象，把接收自客户端的所有请求都交给这个对象处理
@@ -12,13 +12,13 @@ manager = Manager(app)
 # 拦截路由
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 # 动态路由
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name=name)  # 第一个参数是模板的文件名，随后的参数都是键值对
 
 
 if __name__ == '__main__':
