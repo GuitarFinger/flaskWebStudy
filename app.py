0f7861f3482e566fa7ea396b16eb0cad882b66ca
HZC <815547360@qq.com>
2017-06-01 01:33:28 +0800

    Web forms with Flask-WTF(4a)

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*-
 __author__ = 'HZC'
-from datetime import datetime
 from flask import Flask, render_template  # 渲染模板模块render_template
 from flask_script import Manager  # 为flask程序添加了一个命令行解析器
 from flask_bootstrap import Bootstrap  # 导入bootstrap框架
 from flask_moment import Moment  # 本地化日期和时间
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)  # 程序实例是Flask类的对象，把接收自客户端的所有请求都交给这个对象处理
+app.config['SECRET_KEY'] = 'hard to guess string'  # 设置密钥
 
 manager = Manager(app)  # manager实例
 bootstrap = Bootstrap(app)  # bookstrap实例
@@ -15,6 +18,11 @@ moment = Moment(app)
 
 
 # -------------------拦截路由---------------------
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 # 404页面:客户端请求位置页面或路由时显示
 @app.errorhandler(404)
@@ -28,15 +36,15 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-
-# 动态路由
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)  # 第一个参数是模板的文件名，随后的参数都是键值对
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        print(form.name)
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 if __name__ == '__main__':
