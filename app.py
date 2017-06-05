5b23f3167a3cce7929f16dc73c99962233ab4cb5
HZC <815547360@qq.com>
2017-06-05 13:30:05 +0800

    Chapter 5:Database medels with Flask-SQLAlchemy(5a)

--- app.py
+++ app.py
@@ -6,29 +6,58 @@ __author__ = 'HZC'
 # redirect 重定向，参数是重定向的URL
 # url_for URL生成函数,第一个参数是端点名，是相应视图函数的名字
 # flash 提示消息 使用get_flashed_messages() 函数开放给模板,用来获取并渲染消息
+import os  # 操作系统模块
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager  # 为flask程序添加了一个命令行解析器
 from flask_bootstrap import Bootstrap  # 导入bootstrap框架
 from flask_moment import Moment  # 本地化日期和时间
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm  # 表单
+from wtforms import StringField, SubmitField  # 表单输入框和提交按钮
+from wtforms.validators import DataRequired  # 表单校验 数据校验
+from flask_sqlalchemy import SQLAlchemy  # 数据库
+
+basedir = os.path.abspath(os.path.dirname(__file__))  # 返回脚本的路径
 
 app = Flask(__name__)  # 程序实例是Flask类的对象，把接收自客户端的所有请求都交给这个对象处理
 app.config['SECRET_KEY'] = 'hard to guess string'  # 设置密钥
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')  # 保存数据库url
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 每次请求结束后都会自动提交数据库中得变动
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-manager = Manager(app)  # manager实例
-bootstrap = Bootstrap(app)  # bookstrap实例
+# 实例对象
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 
-# -------------------拦截路由---------------------
+# ----------------------类-----------------------
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
+# -------------------拦截路由---------------------
 # 404页面:客户端请求位置页面或路由时显示
 @app.errorhandler(404)
 def page_not_found(e):
@@ -54,4 +83,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db.create_all()
     manager.run()
